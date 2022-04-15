03bca0d3b6577498ce7497a84580be6184a57705
Pietra Aparecida Cardoso <ravennapac@gmail.com>
2022-04-15 10:22:21 -0300

    Alteração na splashscreen

--- src/components/Splashscreen/styles.ts
+++ src/components/Splashscreen/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 1366px;
+  width: 100vw;
+  height: 100vh;
   background: #000000;
   display: flex;
   align-items: center;
